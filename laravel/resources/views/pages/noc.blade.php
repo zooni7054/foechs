44e008c7cdcf25f635526f6cb08a38108dcd45d1
Imran Hussain <zooni7054@gmail.com>
2021-12-07 13:43:44 +0500

    nocs

--- laravel/resources/views/pages/noc.blade.php
+++ laravel/resources/views/pages/noc.blade.php
@@ -16,9 +16,63 @@
 
     <div class="clearfix"></div>
 
-    <div class="section under-construction">
+    <div class="section nocs">
         <div class="container">
-            <h2>Under Construction</h2>
+
+            <div class="row">
+
+                <div class="col-sm-6 col-md-4">
+                    <a href="{{ asset('uploads/documents/61ae68ec8be4f.pdf') }}" target="_blank">
+                        <div class="item" style="background-image: url({{ asset('img/noc/event.jpg') }});">
+                            <div class="content-area">
+                                <h4>NOC from RDA</h4>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                    <a href="{{ asset('uploads/documents/61ae691919e0a.pdf') }}" target="_blank">
+                        <div class="item" style="background-image: url({{ asset('img/noc/event.jpg') }});">
+                            <div class="content-area">
+                                <h4>NOC from RDA Layout Plan</h4>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                    <a href="{{ asset('uploads/documents/61ae694062906.pdf') }}" target="_blank">
+                        <div class="item" style="background-image: url({{ asset('img/noc/event.jpg') }});">
+                            <div class="content-area">
+                                <h4>NOC from AHQ</h4>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                    <a href="{{ asset('uploads/documents/61ae6976d3021.pdf') }}" target="_blank">
+                        <div class="item" style="background-image: url({{ asset('img/noc/event.jpg') }});">
+                            <div class="content-area">
+                                <h4>NOC Environment</h4>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                    <a href="{{ asset('uploads/documents/61ae6998e2831.pdf') }}" target="_blank">
+                        <div class="item" style="background-image: url({{ asset('img/noc/event.jpg') }});">
+                            <div class="content-area">
+                                <h4>NOC Civil Aviation</h4>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+
         </div>
     </div>
 
